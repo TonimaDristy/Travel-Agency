28bc095a7c759d1e01ee8555e11a264626da0c8e
TonimaDristy <tidristy477@gmail.com>
2025-12-17 01:15:32 +0600

    fortawesome

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    Faphone,
+    faEnvelope,
+    faUser,
+    faBars,
+    faTimes,
+
+
+} from '@fortawesome/free-brands-svg-icons'
+
+
+
+
+
 export default function Nav() {
     return (
         <div>Nav</div>
