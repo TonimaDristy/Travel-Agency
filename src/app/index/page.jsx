63278e1adf74cb0a6a795a8844b381fafcb3bebf
TonimaDristy <tidristy477@gmail.com>
2025-12-17 19:57:44 +0600

    indexfucn

--- src/app/index/page.jsx
+++ src/app/index/page.jsx
@@ -12,7 +12,15 @@ import Link from "next/link";
 export default function Index() {
     return (
         <>
-            <h1> This is Index</h1>
+            <div className="hero h-screen min-h-screen flex justify-items-center items-center z-10">
+                <div className="hero-content relative text-center">
+                    <h1 className="xl:text-8xl lg:text-7xl md:text-6xl text-4xl unbounded-font font-bold text-white">
+                        Find Your Best <br /> travel Package
+                    </h1>
+
+
+                </div>
+            </div>
         </>
     )
 }
