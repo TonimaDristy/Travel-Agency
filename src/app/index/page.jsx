16feebd5494d60e706ffae52a47c25b6b44cd8ad
TonimaDristy <tidristy477@gmail.com>
2025-12-17 19:51:03 +0600

    index imports

--- src/app/index/page.jsx
+++ src/app/index/page.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+"use client"
 
-export default function page() {
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-brands-svg-icons";
+
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Index() {
     return (
         <>
             <h1> This is Index</h1>
