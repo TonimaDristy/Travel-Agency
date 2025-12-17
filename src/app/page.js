1d0ef08e0211f1bb0d48b071c3e45ae78ad92e56
TonimaDristy <tidristy477@gmail.com>
2025-12-17 17:04:09 +0600

    index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 //import Nav from "@/components/Nav";  // works if @ points to src/
 import Nav from "./Components/Nav";
+import Index from "./Index/page";
 
 export default function Home() {
   return (
 
-    <main >
-    </main>
+    <>
+      <index />
+    </>
 
   );
 }
