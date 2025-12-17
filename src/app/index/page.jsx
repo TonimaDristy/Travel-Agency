1db1d809c10fabdcce16f49a06a8b16bd7bc12ca
TonimaDristy <tidristy477@gmail.com>
2025-12-17 20:07:01 +0600

    offerline

--- src/app/index/page.jsx
+++ src/app/index/page.jsx
@@ -15,9 +15,11 @@ export default function Index() {
             <div className="hero h-screen min-h-screen flex justify-items-center items-center z-10">
                 <div className="hero-content relative text-center">
                     <h1 className="xl:text-8xl lg:text-7xl md:text-6xl text-4xl unbounded-font font-bold text-white">
-                        Find Your Best <br /> travel Package
+                        Plan Your Dream <br /> Vacation Today
                     </h1>
-
+                    <p className="pt-3 text-blue-300 md:pb-10 pb-5">
+                        Discover Unbeatable Travel Deals Across the World.Luxury, Comfort, and Adventure—All in One Package
+                    </p>
 
                 </div>
             </div>
