1963acdef04cc580579e7645ae91dd7646563d50
TonimaDristy <tidristy477@gmail.com>
2025-12-27 18:18:07 +0600

    newpagjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
-//import Nav from "@/components/Nav";  // works if @ points to src/
-import Nav from "./Components/Nav";
-import Index from "./Index/page";
-
 export default function Home() {
   return (
+    <section className="hero flex items-center justify-center text-center">
+      <div>
+        <p className="text-blue-300 mb-6">
+          Discover Unbeatable Travel Deals Across the World. Luxury, Comfort,
+          and Adventure—All in One Package
+        </p>
 
-    <>
-      <Index />
-    </>
-
+        <button className="bg-blue-500 text-white px-8 py-4 rounded-full font-bold">
+          VIEW ALL TOURS
+        </button>
+      </div>
+    </section>
   );
 }
